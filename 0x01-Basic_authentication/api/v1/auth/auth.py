15a50e514a2f9f20200f3ec9b0d90e39ec5b8056
KagisoMakgaba1 <kagisomakgaba99@gmail.com>
2024-09-03 08:15:26 +0000

    all

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,21 @@
+# api/v1/auth/auth.py
+
+"""This module implements base class for Authentication mechanisms."""
+
+from flask import request
+from typing import List, TypeVar
+
+class Auth:
+    """Template for managing API authentication"""
+    
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Determines if authentication is required - currently always returns False"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """Returns the value of the Authorization header - currently returns None"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Returns the current user - currently returns None"""
+        return None
