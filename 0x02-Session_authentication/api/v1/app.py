4db91d64e670dc083d7d107fc0d61ca3cd35d9a2
KagisoMakgaba1 <kagisomakgaba99@gmail.com>
2024-09-04 21:15:55 +0000

    all

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -3,69 +3,93 @@
 Route module for the API
 """
 from os import getenv
-from api.v1.views import app_views
-from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
-import os
+from typing import Dict, Tuple
 
+from flask import Flask, abort, jsonify, request
+from flask_cors import CORS
+
+from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-
-
 auth = None
 
 auth_type = getenv("AUTH_TYPE")
-if auth_type == 'basic_auth':
+
+if auth_type == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
+
     auth = BasicAuth()
-elif auth_type:
+elif auth_type == "auth":
     from api.v1.auth.auth import Auth
+
     auth = Auth()
+elif auth_type == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
+
+    auth = SessionAuth()
+elif auth_type == "session_exp_auth":
+    from api.v1.auth.session_exp_auth import SessionExpAuth
+
+    auth = SessionExpAuth()
+elif auth_type == "session_db_auth":
+    from api.v1.auth.session_db_auth import SessionDBAuth
+
+    auth = SessionDBAuth()
+
+
+@app.errorhandler(404)
+def not_found(_) -> Tuple[Dict, int]:
+    """Not found handler"""
+    return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
-def unauthorized(error) -> str:
-    """ Unauthorized handler
-    """
+def unauthorized(_) -> Tuple[Dict, int]:
+    """Unauthorized error handler."""
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
-def forbidden(error) -> str:
-    """ Forbidden handler
-    """
+def forbidden(_) -> Tuple[Dict, int]:
+    """Forbidden API operation handler."""
     return jsonify({"error": "Forbidden"}), 403
 
 
-@app.errorhandler(404)
-def not_found(error) -> str:
-    """ Not found handler
-    """
-    return jsonify({"error": "Not found"}), 404
-
-
 @app.before_request
-def before_request_handler():
-    """Handler that runs before each request to filter out
-    unauthorized access."""
+def auth_middleware():
+    """Before request handler."""
     if auth is None:
-        return
+        return None
 
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+    excluded_paths = [
+        "/api/v1/status/",
+        "/api/v1/unauthorized/",
+        "/api/v1/forbidden/",
+        "/api/v1/auth_session/login/",
+    ]
+
     if not auth.require_auth(request.path, excluded_paths):
-        return
+        return None
 
-    if auth.authorization_header(request) is None:
+    auth_header = auth.authorization_header(request)
+    session_cookie = auth.session_cookie(request)
+
+    if auth_header and session_cookie:  # one auth type at a time.
+        return None
+
+    if not auth_header and not session_cookie:  # no auth type provided.
         abort(401)
 
-    if auth.current_user(request) is None:
+    user = auth.current_user(request)
+    if user is None:
         abort(403)
+
+    request.current_user = user
 
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
+    app.run(host=host, port=port, debug=getenv("DEBUG", False))
