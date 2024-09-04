dbdaad44f5fe8bc78241bfe0be7bb605870b2639
KagisoMakgaba1 <kagisomakgaba99@gmail.com>
2024-09-04 21:23:51 +0000

    all

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -15,8 +15,11 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 auth = None
-
 auth_type = getenv("AUTH_TYPE")
+
+if auth_type == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 if auth_type == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
